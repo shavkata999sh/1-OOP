c6c63b1aa1ea588dbe46a880f5cc39fe3035c7c7
shavkata999sh <156001276+shavkata999sh@users.noreply.github.com>
2024-01-10 22:47:42 +0500

    Add files via upload

---
+++ CarLoyihasi.java
@@ -0,0 +1,19 @@
+public class CarLoyihasi {
+    public static void main(String[] args){
+        Car BMW = new Car();
+        System.out.println("<<BMW haqida malumotlar>>");
+        BMW.getrangi("Qora");
+        BMW.getmodel("BMW super car MP5");
+        BMW.getnarxi("120000$");
+        BMW.getAvtomatyoMexanika("Avtamat\n");
+
+
+        Car MERS = new Car();
+        System.out.println("<<MErs haqida malumot>>");
+        MERS.getrangi("Qora");
+        MERS.getmodel("MERS550");
+        MERS.getnarxi("100000$");
+        MERS.getAvtomatyoMexanika("Mexanika");
+
+    }
+}
